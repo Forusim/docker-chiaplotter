64405371351471cb92e26376fd712c5e3ce60379
Forusim <forusim@web.de>
2021-07-09 23:20:00 +0200

    Replace -p with -c
    Nobody will create OG plots anymore

--- start.sh
+++ start.sh
@@ -14,7 +14,7 @@ if [[ -v t2 ]]; then CMD+="-2 ${t2} "; fi
 
 if [[ -v d ]]; then CMD+="-d ${d} "; fi
 
-if [[ -v p ]]; then CMD+="-p ${p} "; fi
+if [[ -v c ]]; then CMD+="-c ${c} "; fi
 
 if [[ -v f ]]; then CMD+="-f ${f} "; fi
 
